bb59d6d0fa36d72814c358a5282a7d573ea7dd82
unknown <Administrator@Preetika-lp.ssdiblr.sharp.co.in>
2021-03-05 17:41:22 +0530

    honeywell-test

---
+++ DateTimeZoneConversion/DateTimeZoneConversion/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeZoneConversion
+{
+    class Program
+    {
+       /// <summary>
+       /// main method
+       /// </summary>
+       /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            //get current time in utc format
+            DateTime currentDateTime = DateTime.UtcNow;
+
+            //read input time zone
+            string inputType = "";
+            Console.WriteLine("Enter Required Time Zone:");
+            inputType = Console.ReadLine();
+
+            //convert input time zone into full string
+            string fullTimeZone = ConvertZoneIdToString(inputType.ToLower());
+
+            //convert current time to specified input time zone
+            DateTime finalDateTime = default(DateTime);
+            if (!fullTimeZone.Equals(""))
+                finalDateTime = GetSpecificDateTime(currentDateTime, fullTimeZone);
+            else
+                Console.WriteLine("Invalid Time zone");
+
+            //display converted date time
+            if (!finalDateTime.Equals(default(DateTime)))
+                Console.WriteLine("Converted Date Time: " + string.Format("{0:MM/dd/yyyy hh:mm:ss tt}", finalDateTime));
+            Console.ReadLine();
+        }
+
+        private static string ConvertZoneIdToString(string inputZone)
+        {
+            string inputFullZone = "";
+            switch (inputZone)
+            {
+                case "est":
+                    inputFullZone = "Eastern Standard Time";
+                    break;
+                case "cst":
+                    inputFullZone = "Central Standard Time";
+                    break;
+                case "pst":
+                    inputFullZone = "Pacific Standard Time";
+                    break;
+                case "ist":
+                    inputFullZone = "India Standard Time";
+                    break;
+                default:
+                    break;
+            }
+            return inputFullZone;
+        } 
+        private static DateTime GetSpecificDateTime(DateTime currentDateTime, string inputFullZone)
+        {
+            DateTime convertedTime = default(DateTime);
+            try
+            {
+                TimeZoneInfo inputZone = TimeZoneInfo.FindSystemTimeZoneById(inputFullZone);
+                convertedTime = TimeZoneInfo.ConvertTimeFromUtc(currentDateTime, inputZone);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error in conversion: " + ex.Message);
+            }
+
+            return convertedTime;
+        }
+    }
+
+}
---
+++ DateTimeZoneConversion/Sorting/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> lstLength = new List<int>();
+            //input 
+            String[] inputItem = { "1101A", "1000", "AB100", "1100", "3001", "2000A", "3500", "2000" };
+
+            //declare dictionary to hold strings of equal length
+            Dictionary<int, List<string>> dictOfString = new Dictionary<int, List<string>>();
+
+            //put strings of same length  to same key in dictionary
+            foreach (var item in inputItem)
+            {
+                int itemLen = item.Length;
+                if (dictOfString.ContainsKey(itemLen))
+                {
+                    dictOfString[itemLen].Add(item);
+                }
+                else
+                {
+                    lstLength.Add(itemLen);
+                    List<string> lst = new List<string>();
+                    lst.Add(item);
+                    dictOfString[itemLen] = lst;
+                }
+            }
+
+            //sorting strings of equal length in dictionary
+            foreach (var item in dictOfString)
+            {
+                item.Value.Sort();
+            }
+            // sorting to get sorted keys in dictionary
+            lstLength.Sort();
+
+
+            //display sorted list
+            Console.Write("Sorted items :{ ");
+            foreach (var item in lstLength)
+            {
+                foreach (var sortedItem in dictOfString[item])
+                {
+                    Console.Write("\"" + sortedItem + "\" ");
+                }
+
+            }
+            Console.WriteLine(" }");
+
+            Console.ReadLine();
+        }
+
+    }
+}
